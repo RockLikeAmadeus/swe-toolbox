a5ca2ab0d27788e003e4b9e05b8b6f0903d51e4f
Alec Hampton <admin@thewebfromscratch.com>
2023-12-22 15:08:44 -0700

    React reference progress

--- front-end-reference/React/appointments-example-app/test/reactTestExtensions.js
+++ front-end-reference/React/appointments-example-app/test/reactTestExtensions.js
@@ -8,6 +8,19 @@ export const initializeReactContainer = () => {
   document.body.replaceChildren(container);
 };
 
+// Import this to replace calls to `describe` for react components.
+// Simplifies test code by performing initialization within.
+export const describeReactComponent = (componentName, fn) => {
+  fnWrapper = () => {
+    beforeEach(() => {
+      initializeReactContainer();
+    });
+    fn();
+  };
+
+  describe(componentName, fnWrapper);
+};
+
 // Import this to simplify rendering your component in your test
 export const render = (component) =>
   act(() => {
@@ -21,16 +34,16 @@ export const render = (component) =>
 // click(button);
 export const click = (element) => act(() => element.click());
 
-// Import this to replace calls to `describe` for react components.
-// Simplifies test code by performing initialization within.
-export const describeReactComponent = (componentName, fn) => {
-  fnWrapper = () => {
-    beforeEach(() => {
-      initializeReactContainer();
-    });
+// Wraps document.querySelector()
+export const getElement = (selector) => document.querySelector(selector);
 
-    fn();
-  };
+// Wraps document.querySelectorAll() and returns an array
+export const getElements = (selector) =>
+  Array.from(document.querySelectorAll(selector));
 
-  describe(componentName, fnWrapper);
-};
+// Maps an array of elements to an array of their types
+export const typesOf = (elements) => elements.map((element) => element.type);
+
+// Maps an array of elements to an array of their text contents
+export const textOf = (elements) =>
+  elements.map((element) => element.textContent);
